3a73c084ba428b6d661f12fb9384425fedcc1342
LiveReporting <livereporting@kushals-macbook.local>
2015-10-30 08:46:04 +0545

    Initial Commit

---
+++ Live_reporting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Live_reporting
+//
+//  Created by LiveReporting on 10/30/15.
+//  Copyright © 2015 LiveReporting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Live_reportingTests/Live_reportingTests.swift
@@ -0,0 +1,36 @@
+//
+//  Live_reportingTests.swift
+//  Live_reportingTests
+//
+//  Created by LiveReporting on 10/30/15.
+//  Copyright © 2015 LiveReporting. All rights reserved.
+//
+
+import XCTest
+@testable import Live_reporting
+
+class Live_reportingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
