623e7568fe30321697645c55f1eb14f71d8966a5
janammaharjan <janammaharjan@gmail.com>
2015-11-06 13:36:25 +0545

    Video play on annotation selection

--- videoView.swift
+++ videoView.swift
@@ -1,10 +1,4 @@
-//
-//  videoView.swift
-//  Live_reporting
-//
-//  Created by user on 5/11/15.
-//  Copyright © 2015 LiveReporting. All rights reserved.
-//
+
 
 import UIKit
 import MediaPlayer
@@ -12,16 +6,23 @@ import MediaPlayer
 class videoView: UIViewController {
 
     
+    var receivedString: String = ""
+    
     @IBOutlet weak var vwVideo: UIView!
         var moviePlayer:MPMoviePlayerController! = MPMoviePlayerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        
+        let url:NSURL = NSURL(string: receivedString)!
         
        // let url:NSURL = NSURL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")!
         
-        let url:NSURL = NSURL(string: "http://files.parsetfss.com/f4a09e86-6d45-4841-b3f7-0e983178a5d6/tfss-32d732a7-f1f0-40a7-9847-d615f9a0ad6c-chelseaSong.mp4")!
+       // let url = NSURL(string: "http://files.parsetfss.com/f4a09e86-6d45-4841-b3f7-0e983178a5d6/tfss-32d732a7-f1f0-40a7-9847-d615f9a0ad6c-chelseaSong.mp4")!
+        
         moviePlayer = MPMoviePlayerController(contentURL: url)
+        
         
         
         
@@ -35,7 +36,7 @@ class videoView: UIViewController {
         moviePlayer.prepareToPlay()
         moviePlayer.play()
         
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
